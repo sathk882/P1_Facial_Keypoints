40b74e5d9c73ddfcfe2c6324ef0f4b641d13bdbc
Satheesh K <satheeshkatipomu@gmail.com>
2019-03-08 22:52:27 +0530

    Committing CNN Architecture
    Defined CNN Architecture for facial points detection.

--- models.py
+++ models.py
@@ -1,7 +1,6 @@
 ## TODO: define the convolutional neural network architecture
 
 import torch
-from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 # can use the below import should you choose to initialize the weights of your Net
@@ -20,7 +19,14 @@ class Net(nn.Module):
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
+        self.conv1 = nn.Conv2d(1, 32, 7)
+        self.conv2 = nn.Conv2d(32,64,5)
+        self.conv3 = nn.Conv2d(64,128,3)
+        self.conv4 = nn.Conv2d(128,256,2)
+        self.pool = nn.MaxPool2d(2,2)
+        self.fc1 = nn.Linear(256*12*12,576)
+        self.fc2 = nn.Linear(576,255)
+        self.fc3 = nn.Linear(255,136)
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
@@ -30,7 +36,23 @@ class Net(nn.Module):
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
-        ## x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv1(x)))
+        x = F.dropout2d(x,p=0.3)
+        x = self.pool(F.relu(self.conv2(x)))
+        x = F.dropout2d(x,p=0.3)
+        x = self.pool(F.relu(self.conv3(x)))
+        x = F.dropout2d(x,p=0.4)
+        x = self.pool(F.relu(self.conv4(x)))
+        x = F.dropout2d(x,p=0.4)
+        x = x.view(x.size(0), -1)
+        #print(x.shape)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = F.dropout2d(x,p=0.5)
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = F.dropout2d(x,p=0.5)
+        x = self.fc3(x)
         
         
         # a modified x, having gone through all the layers of your model, should be returned
